860727cfb827cce83ffa8b8a206e2ab1670f6c5a
Naiyarah <naiyarah.h@gmail.com>
2022-06-30 16:47:27 +0400

    Tested Sign in

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,7 +1,18 @@
+import React from "react";
+
 export default function SignIn() {
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("/sign-in")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+
   return (
     <div>
       <h1>Sign in </h1>
+      <p>{!data ? "Loading..." : data}</p>
     </div>
   );
 }
