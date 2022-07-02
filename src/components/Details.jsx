40434aef22b69a75216c9acce6836ade97c975f1
djtanner <zshariat08@gmail.com>
2022-07-02 14:01:31 +0400

    Implemented carbonApion tripdetails page

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -8,14 +8,36 @@ import style from "../styles/Details.module.scss";
 
 
 const Details = () => {
-  const {
+  let output;
+
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '',
+      'X-RapidAPI-Host': 'carbonfootprint1.p.rapidapi.com'
+    }
+  };
+  
+
+
+  const { register,
     handleSubmit,
     formState: { errors },
   } = useForm();
 
   const onSubmit = (data) => {
     console.log(data);
+    let vehicle = data.size + data.fuel + 'Car'
+    //let distance = data.distance.toString()
+    console.log(vehicle)
+
+    fetch(`https://carbonfootprint1.p.rapidapi.com/CarbonFootprintFromCarTravel?distance=${data.distance}&vehicle=${vehicle}`, options)
+    .then(response => response.json())
+    .then(response => {console.log(response); output = response.carbonEquivalent; return console.log(output);})
+    .catch(err => console.error(err));
   };
+
+
   return (
     <>
       <header className="header">
@@ -35,14 +57,14 @@ const Details = () => {
 
       <form onSubmit={handleSubmit(onSubmit)}>
     <label>Car Size: </label>
-    <select id="size">
+    <select id="size" {...register("size")}>
     <option value="Small">Small</option>
     <option value="Medium">Medium</option>
     <option value="Large">Large</option>
     </select>
 
     <label>Fuel Type:</label>
-    <select id="fuel">
+    <select id="fuel" {...register("fuel")}>
     <option value="Petrol">Petrol</option>
     <option value="Diesel">Diesel</option>
     <option value="Hybrid">Hybrid</option>
@@ -51,12 +73,15 @@ const Details = () => {
     </select>
 
 
-
-        <Input label="Distance in km:" type="number" id="distance" />
+        <label>Distance in km:</label>
+        <input type = "number" id="distance" {...register("distance")} />
+  
         <div className={style.subBtn}>
-          <Button text="Submit" to="/history" />
+        <button type="submit">Submit</button>
         </div>
       </form>
+{/*this code isn't working*/}
+      {output && <h2>Your carbon equivalent for this trip is {output} t CO2.</h2>}
       <Footer />
     </>
   );
